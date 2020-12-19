c1b5590aab3a4ab21299dfc4cc3326317d55713e
CriusNyx <rjreynoldsw@gmail.com>
2020-12-19 15:48:20 -0500

    Time Mechanic Implemented

---
+++ Assets/Time/GameTime.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TimeBehaviour : MonoBehaviour
+{
+    private List<(float time, Func<IEnumerator> func)> actions = new List<(float time, Func<IEnumerator> func)>();
+
+    protected void RegisterAction(float time, Func<IEnumerator> func)
+    {
+        this.actions.Add((time, func));
+    }
+
+    protected void RegisterAction(float minutes, float seconds, Func<IEnumerator> func) => RegisterAction(new TimeStamp(minutes, seconds), func);
+
+    protected void RegisterAction(TimeStamp time, Func<IEnumerator> func)
+    {
+        this.actions.Add((time.GetTime(), func));
+    }
+
+    private void FixedUpdate()
+    {
+        List<(float time, Func<IEnumerator> func)> actionsToRemove = new List<(float time, Func<IEnumerator> func)>();
+
+        foreach(var action in actions)
+        {
+            if(action.time < Time.timeSinceLevelLoad)
+            {
+                actionsToRemove.Add(action);
+                StartCoroutine(action.func());
+            }
+        }
+
+        foreach(var action in actionsToRemove)
+        {
+            actions.Remove(action);
+        }
+    }
+
+    protected abstract void ProtectedFixedUpdate();
+}
---
+++ Assets/Time/TimeStamp.cs
@@ -0,0 +1,16 @@
+﻿public class TimeStamp
+{
+    public readonly float minutes;
+    public readonly float seconds;
+
+    public TimeStamp(float minutes, float seconds)
+    {
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public float GetTime()
+    {
+        return minutes * 60f + seconds;
+    }
+}
