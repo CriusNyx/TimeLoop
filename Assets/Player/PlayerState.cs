1d3d62a6b2567e1a583224ed7bcc0fb2078b4ca2
CriusNyx <rjreynoldsw@gmail.com>
2020-12-19 18:18:04 -0500

    Player Movement Refactor

---
+++ Assets/Player/PlayerState.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerState
+{
+    public Player player;
+
+    public Rigidbody rigidbody;
+    public Vector3 velocity = Vector3.zero;
+    public bool groundedLastFrame = false;
+
+    public InputBuffer buffer = new InputBuffer();
+
+
+    public PlayerState(Player player, Rigidbody rigidbody)
+    {
+        this.player = player;
+        this.rigidbody = rigidbody;
+    }
+}
