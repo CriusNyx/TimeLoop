c4a87193a3b878ea1d3982c72ae6b117d6b03484
CriusNyx <rjreynoldsw@gmail.com>
2020-12-19 15:55:27 -0500

    Added example.

---
+++ Assets/Time/TimeBehaviourExample.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBehaviourExample : TimeBehaviour
+{
+    private void Start()
+    {
+        // Routine to do will execute in 0 minutes and 1 second.
+        RegisterAction(0f, 1f, RoutineToDo);
+    }
+
+    private IEnumerator RoutineToDo()
+    {
+        Debug.Log("I'm doing the thing.");
+        // This behaviour will execute after a certain time once registered.
+        yield return null;
+    }
+
+    protected override void ProtectedFixedUpdate()
+    {
+
+    }
+}
