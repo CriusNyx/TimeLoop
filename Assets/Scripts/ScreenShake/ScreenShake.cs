d80bc5287a01524c083cfbaa4ea52bd40d70ea7d
CriusNyx <rjreynoldsw@gmail.com>
2020-12-21 12:50:14 -0500

    Screen Shake

--- Assets/Scripts/ScreenShake/ScreenShake.cs
+++ Assets/Scripts/ScreenShake/ScreenShake.cs
@@ -11,8 +11,10 @@ public class ScreenShake : TimeBehaviour
         large
     }
 
-    private float thetaAt1 = 30f;
-
+    private float thetaAt1 = 15f;
+    private float perlinSpeed = 5f;
+    private const int xOffset = 10000;
+    private const int yOffset = 1000000;
     private float myShake = 0f;
 
     public static float ShakeAmountToFloat(ShakeAmount amount)
@@ -35,9 +37,43 @@ public class ScreenShake : TimeBehaviour
         shake.myShake = Mathf.Max(shake.myShake, f);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Shake(ShakeAmount.small);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Shake(ShakeAmount.medium);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Shake(ShakeAmount.large);
+        }
+    }
+
     protected override void ProtectedFixedUpdate()
     {
-        myShake -= Time.fixedDeltaTime;
+        float shakeValue = myShake * myShake;
 
+        float x = Mathf.PerlinNoise(xOffset + Time.time * perlinSpeed, 0f);
+        float y = Mathf.PerlinNoise(yOffset + Time.time * perlinSpeed, 0f);
+
+        x = x * 2 - 1;
+        y = y * 2 - 1;
+
+        Quaternion localRot = Quaternion.Euler(
+            x * shakeValue * thetaAt1, 
+            y * shakeValue * thetaAt1, 
+            0f);
+
+        foreach(Transform child in transform)
+        {
+            child.localRotation = localRot;
+        }
+
+        myShake -= Time.fixedDeltaTime;
+        myShake = Mathf.Clamp(myShake, 0f, myShake);
     }
 }
